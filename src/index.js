78d811a40ae65995f1056e412e0b1d971d4554e1
Vlad-Moskalenko <94741971+Vlad-Moskalenko@users.noreply.github.com>
2022-07-25 21:09:46 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsObj = {}
+  let arr = []
+
+  bracketsConfig.forEach(item=>{
+    let key = item[0];
+    let value = item[1];
+    bracketsObj[key] = value;
+  })
+ 
+  if(str.length % 2 !==0){return false}
+
+  str.split('').forEach(item=> {
+    if(arr.length !==0 && bracketsObj[arr[arr.length-1]] === item) {arr.pop()}
+    else if(bracketsObj[item]) {arr.push(item)}
+    else {
+      return false
+    }
+})
+    
+  return arr.length === 0
 }
